de0ce2126246cfc3797c9aa07ea6ad231b215b79
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-28 15:22:01 -0700

    Refine rootReducer

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import currentVideo from './currentVideo.js';
 import videoList from './videoList.js';
 
-const defaultState = { currentVide: null, videoList: [] };
+var defaultState = { currentVide: null, videoList: [] };
 
 var rootReducer = function combination(state = defaultState, action) {
   return combineReducers({ currentVideo, videoList })(state, action);
