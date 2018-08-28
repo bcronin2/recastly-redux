4a795a05e5d217725e985d329d370eff0d8854e5
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-28 16:54:03 -0700

    Remove unneeded methods from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,37 +4,6 @@ import VideoPlayerContainer from '../containers/VideoPlayerContainer.js';
 import Nav from './Nav.js';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      videos: [],
-      currentVideo: null
-    };
-  }
-
-  // componentDidMount() {
-  //   this.getYouTubeVideos('react tutorials');
-  // }
-
-  // handleVideoListEntryTitleClick(video) {
-  //   this.setState({ currentVideo: video });
-  // }
-
-  // getYouTubeVideos(query) {
-  //   var options = {
-  //     key: this.props.API_KEY,
-  //     query: query
-  //   };
-
-  //   this.props.searchYouTube(options, videos =>
-  //     this.setState({
-  //       videos: videos,
-  //       currentVideo: videos[0]
-  //     })
-  //   );
-  // }
-
   render() {
     return (
       <div>
